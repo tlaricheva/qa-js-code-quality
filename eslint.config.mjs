7562135c65ac4cd0fda1922bd7c5142acdf9cc3c
Tania <tl303132@gmail.com>
2025-12-19 10:35:12 +0100

    setup eslint, prettier and pre-commit hook

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,13 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
+import prettier from 'eslint-config-prettier';
 
 export default defineConfig([
   {
-    files: ["**/*.{js,mjs,cjs}"],
+    files: ['**/*.{js,mjs,cjs}'],
     plugins: { js },
-    extends: ["js/recommended"],
+    extends: ['js/recommended', prettier],
     languageOptions: {
       globals: globals.node,
     },
